cb802ccdbb437aa8f71eab1bcd1099b623e24ccc
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-12-01 20:16:54 +0100

    This is the day two task

---
+++ day_2/variables.py
@@ -0,0 +1,56 @@
+#Exercises: Level 1 
+## Day 2: 30 Days of python programming
+first_name = "Sheriff"
+last_name = "Mudasir"
+full_name = "Sheriff Olalekan Mudasir"
+country = "Nigeria"
+city = "Kano"
+age = "25"
+year = 2023
+is_married = True
+is_true = True
+is_light = True
+x1, x2 = [5, 7]
+print(x1)
+
+#Exercises: Level 2
+## No. 1
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light))
+##No. 2
+print(len(first_name))
+##No. 3
+print(len(first_name) == len(last_name))
+##No. 4
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+## No. 5
+PI  = 3.142
+r = 30
+area_of_circle = PI * r ** 2
+circum_of_circle = 2 * PI * r
+r = input("What is the radius of the circle: ")
+r = int(r)
+area = r ** 2 * PI
+print("The area of the circle is ", area)
+##No. 6
+firstname = input("What is your first name: ")
+last_name = input("What is your last name: ")
+country = input("What country are you from?")
+age = input("How old are you ?")
+
+
